35b0015902ef9fe39f02545140009cbeaa5491b5
clemsytoff <80209469+clemsytoff@users.noreply.github.com>
2023-09-07 09:22:00 +0200

    Ajout primitif du Launcher
    Redirection vers le réseau fonctionnel.

---
+++ forge.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        certificateFile: './cert.pfx',
+        certificatePassword: process.env.CERTIFICATE_PASSWORD
+      }
+    }
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'clemsytoff',
+          name: 'linkesportapp',
+          GITHUB_TOKEN: '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'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
+};
